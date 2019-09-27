1ac6224384dd18e2a27c30f96120854dbddb835b
Felix Dhumes <felix@dhumes.fr>
2019-09-27 16:25:59 +0200

    led utils functions

--- led.hpp
+++ led.hpp
@@ -1,3 +1,19 @@
 #include <cstdio>
 #include <string>
 #include <iostream>
+#include <unistd.h>
+
+enum LEDS {ROUGE = 0, VERT, JAUNE, LEDS_NBR};
+const std::string gpioBaseFolder = "/sys/class/gpio/";
+
+std::string ledsNumber[LEDS_NBR];
+std::string leds[LEDS_NBR];
+bool        ledsState[LEDS_NBR];
+
+FILE *fd;
+
+int init_leds();
+void set_led(LEDS led);
+void clear_led(LEDS led);
+void switch_led(LEDS led);
+void blink_led(LEDS led);
