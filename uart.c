cf280b9766bcf6b1bff0ed33acdc8dedd27378d6
Felix Dhumes <felix@dhumes.fr>
2019-10-25 17:23:19 +0200

    succesfull thread tx and rx communication

--- uart.c
+++ uart.c
@@ -3,8 +3,60 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
+#include <stdio.h>
 
 #include "uart.h"
+
+
+#include <pthread.h>
+
+void *receive(void * p) {
+	int fd2 = uart_open("/dev/ttyS0");
+	printf("fd2: %d\n", fd2);
+	char buf[1000];
+		
+	while(1) {
+		int ret = read(fd2, buf, 2);
+		
+		printf("%s ", buf);
+		if(ret == -1) printf("(error)");
+		else printf("no error");
+		printf("\n");
+	}
+	
+	return 0;
+	
+}
+
+void *transmit(void * p) {
+	int fd1 = open("/dev/ttyS0", O_RDWR|O_NONBLOCK);
+	printf("fd1: %d\n", fd1);
+	
+	char c = 'a';
+	
+	while(1) {
+		c = (c + 1 - 'a') % ('z' - 'a') + 'a';
+		write(fd1, &c, 1);
+		//for(int i = 0; i < 10000000; i++);
+	}
+	
+	return 0;
+	
+}
+
+int main() {
+	pthread_t receiver, transmitter;
+	
+	//uart_open("/dev/ttyS0");
+	
+	pthread_create(&transmitter, NULL, transmit, NULL);
+	pthread_create(&receiver, NULL, receive, NULL);
+	
+	while(1);
+	
+	return 0;
+	
+}
 
 int uart_open(char const*filename) 
 {
